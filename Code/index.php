ae63c1bcb612e9bb0597427c853a29e5084dfe84
Romain-len <89740581+romain-lnr@users.noreply.github.com>
2023-02-01 15:36:00 +0100

    Update index.php

--- Code/index.php
+++ Code/index.php
@@ -44,13 +44,5 @@
         </div>
     </form>
 </div>
-<?php
-session_start();
-if (! empty($_SESSION["userId"])) {
-    require_once './view/dashboard.php';
-} else {
-    require_once './view/login-form.php';
-}
-?>
 </body>
 </html>
