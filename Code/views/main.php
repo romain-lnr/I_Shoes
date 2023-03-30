8af2b07d334ca89321dc505a2077f48ddbea6ad0
Romain-len <89740581+romain-lnr@users.noreply.github.com>
2023-03-30 17:10:41 +0200

    Put an animation css zoom

--- Code/views/main.php
+++ Code/views/main.php
@@ -9,6 +9,7 @@
 $title="IShoes - main page";
 ob_start();
 ?>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<div class="topnav">
         <a href="index.php?action=login"><img src="../media/img/login.png" height="50"><br>login</a>
         <a href="index.php?action=login"><img src="../media/img/basket.png" height="50"><br>Basket</a>
@@ -17,35 +18,37 @@ ob_start();
     <?php $topnav = ob_get_clean();
     ob_start();?>
         <br><br>
-        <div class="slideshow-container">
+        <div class="w3-container w3-center w3-animate-zoom">
+            <div class="slideshow-container">
 
-            <div class="mySlides fadeSlide">
-                <div class="numbertext">1 / 3</div>
-                <img src="../media/img/articles/Air%20Jordan%20Dior.png" style="width:100%" height="700">
-                <div class="text">Caption Text</div>
+                <div class="mySlides fadeSlide">
+                    <div class="numbertext">1 / 3</div>
+                    <img src="../media/img/articles/Air%20Jordan%20Dior.png" style="width:100%" height="700">
+                    <div class="text">Caption Text</div>
+                </div>
+
+                <div class="mySlides fadeSlide">
+                    <div class="numbertext">2 / 3</div>
+                    <img src="../media/img/articles/Air%20jordan%204%20off%20white.png" style="width:100%" height="700">
+                    <div class="text">Caption Two</div>
+                </div>
+
+                <div class="mySlides fadeSlide">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="../media/img/articles/Air%20Jordan%20REtro%20High%20TravisScott%20CactusJack.png" style="width:100%" height="700">
+                    <div class="text">Caption Three</div>
+                </div>
+
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
             </div>
+            <br>
 
-            <div class="mySlides fadeSlide">
-                <div class="numbertext">2 / 3</div>
-                <img src="../media/img/articles/Air%20jordan%204%20off%20white.png" style="width:100%" height="700">
-                <div class="text">Caption Two</div>
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
             </div>
-
-            <div class="mySlides fadeSlide">
-                <div class="numbertext">3 / 3</div>
-                <img src="../media/img/articles/Air%20Jordan%20REtro%20High%20TravisScott%20CactusJack.png" style="width:100%" height="700">
-                <div class="text">Caption Three</div>
-            </div>
-
-            <a class="prev" onclick="plusSlides(-1)">❮</a>
-            <a class="next" onclick="plusSlides(1)">❯</a>
-        </div>
-        <br>
-
-        <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
         </div>
     <?php $content = ob_get_clean();
     ob_start();?>
